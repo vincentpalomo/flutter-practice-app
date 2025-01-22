7c36085dc6601ae4f96ef7c52210dd0d3a9c5ec5
Vincent Palomo <vincentpalomo@outlook.com>
2025-01-22 11:47:58 -0600

    starting new page for learning flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,35 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:english_words/english_words.dart';
+import 'package:provider/provider.dart';
 
-import 'features/todos/presentation/pages/todo_page.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'features/todos/presentation/pages/todo_page.dart';
+
+// void main() {
+//   runApp(
+//     const ProviderScope(
+//       child: MyApp(),
+//     ),
+//   );
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Todo App',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: const TodoPage(),
+//     );
+//   }
+// }
 
 void main() {
-  runApp(
-    const ProviderScope(
-      child: MyApp(),
-    ),
-  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -16,12 +37,43 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Todo App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Namer App',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        ),
+        home: MyHomePage(),
       ),
-      home: const TodoPage(),
+    );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    return Scaffold(
+      body: Column(
+        children: [
+          Text('A random AWESOME idea 💡'),
+          Text(appState.current.asLowerCase),
+
+          ElevatedButton(
+              onPressed: () {
+                print('Button pressed!');
+              },
+              child: Text('Next'),
+          ),
+        ],
+      ),
     );
   }
 }
